1d4ed3eda2afa16a1ef4aaf03f9b551f0a5e5b2f
Jeremy Kelber <Bluisk8@Jeremys-iMac.local>
2021-06-19 01:05:51 -0400

    Sixth commit

---
+++ Linux/UpdateUbuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Updates Ubuntu package database  
+sudo apt-get update -y
+
+# Line to visually see the Update and Upgrade Steps
+echo ************************************************************************_
+
+# Installs the Ubuntu updates
+sudo apt-get upgrade -y
+
